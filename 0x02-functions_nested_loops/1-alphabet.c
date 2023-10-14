7cf497d8ad8a5ecbd782a1521c6ce0029fe66be1
HAS20X <H@example.com>
2023-10-14 16:00:16 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void); 
+{
+char print;
+for (print='a'; print<='z'; print++);
+{
+    _putchar(print);
+}
+{
+    _putchar('\n');
+}
+  return 0;
+}
